375082699db6aa79986649bdfc33da302a4377c8
Maryouma <mariem.babi5@gmail.com>
2026-05-03 22:08:08 +0100

    commiting the assignment

---
+++ TP2/Controllers/ProducersController.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TP2.Models.Cinema;
+
+namespace CinemaManager.Controllers
+{
+    public class ProducersController : Controller
+    {
+        CinemaDbContext _context;
+
+        public ProducersController(CinemaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var producers = _context.Producers.ToList();
+            return View(producers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            return View(producer);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Producer producer)
+        {
+            _context.Producers.Add(producer);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            return View(producer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Producer producer)
+        {
+            _context.Producers.Update(producer);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            return View(producer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            _context.Producers.Remove(producer);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TP2/Models/Cinema/CinemaDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP2.Models.Cinema;
+
+public partial class CinemaDbContext : DbContext
+{
+    public CinemaDbContext()
+    {
+    }
+
+    public CinemaDbContext(DbContextOptions<CinemaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<Producer> Producers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CinemaDB;Integrated Security=True;Pooling=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Movie__3214EC076C9D8A9D");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Genre).HasMaxLength(20);
+            entity.Property(e => e.Title).HasMaxLength(30);
+
+            entity.HasOne(d => d.Producer).WithMany(p => p.Movies)
+                .HasForeignKey(d => d.ProducerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Movie_Prod");
+        });
+
+        modelBuilder.Entity<Producer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producer__3214EC07AED4C4E2");
+
+            entity.ToTable("Producer");
+
+            entity.Property(e => e.Email).HasMaxLength(30);
+            entity.Property(e => e.Name).HasMaxLength(30);
+            entity.Property(e => e.Nationality).HasMaxLength(30);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
